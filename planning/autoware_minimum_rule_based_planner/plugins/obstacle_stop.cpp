Owen-Liuyuxuan/autoware.universe

apply per-class lateral margins at obstacle-stop footprint query time
- Replace the scalar corridor margin with a per-class map used at query time
- Expand ego footprints by object label or pointcloud class_id
- Fold pointcloud target types and height limits under objects

Signed-off-by: Mohammad Alqudah <alqudah.mohammad@tier4.jp>

planning/autoware_minimum_rule_based_planner/plugins/obstacle_stop.cpp
@@ -38,6 +38,8 @@ using autoware::trajectory_modifier::utils::replace_trajectory_with_stop_point;
 using autoware::trajectory_modifier::utils::obstacle_stop::build_trajectory_footprint_index;
 using autoware::trajectory_modifier::utils::obstacle_stop::get_nearest_object_collision;
 using autoware::trajectory_modifier::utils::obstacle_stop::get_nearest_pcd_collision;
+using autoware::trajectory_modifier::utils::obstacle_stop::LateralMarginMap;
+using autoware::trajectory_modifier::utils::obstacle_stop::ObjectType;
 using autoware::trajectory_modifier::utils::obstacle_stop::PointCloud;
 
 void ObstacleStop::on_initialize(const MinimumRuleBasedPlannerParams & params)
@@ -186,9 +188,10 @@ std::optional<CollisionPoint> ObstacleStop::check_predicted_objects(
   auto predicted_objects = *data.predicted_objects_ptr;
 
   object_filter_->filter_objects(predicted_objects);
+  const LateralMarginMap lateral_margin_map{{ObjectType::UNKNOWN, params_.lateral_margin}};
   object_filter_->filter_by_target_area(
     predicted_objects, traj_points, context_->vehicle_info, debug_data_.trajectory_shape,
-    params_.lateral_margin, debug_data_.target_polygons);
+    lateral_margin_map, debug_data_.target_polygons);
 
   autoware_perception_msgs::msg::PredictedObject colliding_object;
   auto collision_point = get_nearest_object_collision(
@@ -262,8 +265,9 @@ std::optional<CollisionPoint> ObstacleStop::check_pointcloud(
       filtered_pointcloud, *data.predicted_objects_ptr);
   }
 
+  const LateralMarginMap lateral_margin_map{{ObjectType::UNKNOWN, params_.lateral_margin}};
   return get_nearest_pcd_collision(
-    debug_data_.trajectory_shape, filtered_pointcloud, params_.lateral_margin,
+    debug_data_.trajectory_shape, filtered_pointcloud, lateral_margin_map,
     debug_data_.target_pcd_points);
 }
 

TaikiYamada4/autoware_universe

planning/autoware_minimum_rule_based_planner/plugins/obstacle_stop.cpp
@@ -38,6 +38,8 @@ using autoware::trajectory_processor::utils::replace_trajectory_with_stop_point;
 using autoware::trajectory_processor::utils::obstacle_stop::build_trajectory_footprint_index;
 using autoware::trajectory_processor::utils::obstacle_stop::get_nearest_object_collision;
 using autoware::trajectory_processor::utils::obstacle_stop::get_nearest_pcd_collision;
+using autoware::trajectory_processor::utils::obstacle_stop::LateralMarginMap;
+using autoware::trajectory_processor::utils::obstacle_stop::ObjectType;
 using autoware::trajectory_processor::utils::obstacle_stop::PointCloud;
 
 void ObstacleStop::on_initialize(const MinimumRuleBasedPlannerParams & params)
@@ -186,9 +188,10 @@ std::optional<CollisionPoint> ObstacleStop::check_predicted_objects(
   auto predicted_objects = *data.predicted_objects_ptr;
 
   object_filter_->filter_objects(predicted_objects);
+  const LateralMarginMap lateral_margin_map{{ObjectType::UNKNOWN, params_.lateral_margin}};
   object_filter_->filter_by_target_area(
     predicted_objects, traj_points, context_->vehicle_info, debug_data_.trajectory_shape,
-    params_.lateral_margin, debug_data_.target_polygons);
+    lateral_margin_map, debug_data_.target_polygons);
 
   autoware_perception_msgs::msg::PredictedObject colliding_object;
   auto collision_point = get_nearest_object_collision(
@@ -262,8 +265,9 @@ std::optional<CollisionPoint> ObstacleStop::check_pointcloud(
       filtered_pointcloud, *data.predicted_objects_ptr);
   }
 
+  const LateralMarginMap lateral_margin_map{{ObjectType::UNKNOWN, params_.lateral_margin}};
   return get_nearest_pcd_collision(
-    debug_data_.trajectory_shape, filtered_pointcloud, params_.lateral_margin,
+    debug_data_.trajectory_shape, filtered_pointcloud, lateral_margin_map,
     debug_data_.target_pcd_points);
 }
 